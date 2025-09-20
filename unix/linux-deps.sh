042fc155d1abc8f6f613ba49e97f61c91845f4ca
crueter <crueter@eden-emu.dev>
2025-09-20 16:37:29 -0400

    use arch for linux
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- unix/linux-deps.sh
+++ unix/linux-deps.sh
@@ -1,7 +1,14 @@
 #!/bin/sh -ex
 
-# Explicitly disable man-db auto update (takes forever lol)
-sudo rm /var/lib/man-db/auto-update
-
-sudo apt-get update
-sudo apt install build-essential nasm yasm cmake libc6 libc6-dev unzip wget libnuma1 libnuma-dev clang git libffmpeg-nvenc-dev libva-dev libvulkan-dev
+sudo pacman -Syu --needed --noconfirm \
+    nasm \
+    yasm \
+    cmake \
+    base-devel \
+    git \
+    wget \
+    unzip \
+    gcc \
+    ffnvcodec-headers \
+    vulkan-headers \
+    libva
