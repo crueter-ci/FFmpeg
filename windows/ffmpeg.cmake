7bdc54d80d82e12de80912587a0c7227040cb4aa
crueter <crueter@eden-emu.dev>
2025-09-20 18:39:22 -0400

    Fix cmake file
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- windows/ffmpeg.cmake
+++ windows/ffmpeg.cmake
@@ -1,60 +1,66 @@
-set(FFmpeg_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/include PARENT_SCOPE)
+set(FFMPEG_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/include)
 
 if (BUILD_SHARED_LIBS)
-    set(LIB_TYPE SHARED)
-else()
-    set(LIB_TYPE SHARED)
-endif()
+    set(FFMPEG_LIB_DIR ${CMAKE_CURRENT_LIST_DIR}/bin)
 
-add_library(FFmpeg::swscale ${LIB_TYPE} IMPORTED)
-add_library(FFmpeg::avutil ${LIB_TYPE} IMPORTED)
-add_library(FFmpeg::avcodec ${LIB_TYPE} IMPORTED)
-add_library(FFmpeg::avfilter ${LIB_TYPE} IMPORTED)
+    add_library(swscale  SHARED IMPORTED GLOBAL)
+    add_library(avutil   SHARED IMPORTED GLOBAL)
+    add_library(avcodec  SHARED IMPORTED GLOBAL)
+    add_library(avfilter SHARED IMPORTED GLOBAL)
 
-if (BUILD_SHARED_LIBS)
-    set(FFmpeg_LIBRARY_DIR ${CMAKE_CURRENT_LIST_DIR}/bin PARENT_SCOPE)
-    set_target_properties(FFmpeg::swscale PROPERTIES
-        IMPORTED_LOCATION ${FFmpeg_LIBRARY_DIR}/swscale-SWSCALE_VER.dll
-        IMPORTED_IMPLIB ${FFmpeg_LIBRARY_DIR}/swscale.lib
-        INTERFACE_INCLUDE_DIRECTORIES ${FFmpeg_INCLUDE_DIR}
+    set_target_properties(swscale PROPERTIES
+        IMPORTED_LOCATION ${FFMPEG_LIB_DIR}/swscale-SWSCALE_VER.dll
+        IMPORTED_IMPLIB ${FFMPEG_LIB_DIR}/swscale.lib
+        INTERFACE_INCLUDE_DIRECTORIES ${FFMPEG_INCLUDE_DIR}
     )
 
-    set_target_properties(FFmpeg::avutil PROPERTIES
-        IMPORTED_LOCATION ${FFmpeg_LIBRARY_DIR}/avutil-AVUTIL_VER.dll
-        IMPORTED_IMPLIB ${FFmpeg_LIBRARY_DIR}/avutil.lib
-        INTERFACE_INCLUDE_DIRECTORIES ${FFmpeg_INCLUDE_DIR}
+    set_target_properties(avutil PROPERTIES
+        IMPORTED_LOCATION ${FFMPEG_LIB_DIR}/avutil-AVUTIL_VER.dll
+        IMPORTED_IMPLIB ${FFMPEG_LIB_DIR}/avutil.lib
+        INTERFACE_INCLUDE_DIRECTORIES ${FFMPEG_INCLUDE_DIR}
     )
 
-    set_target_properties(FFmpeg::avcodec PROPERTIES
-        IMPORTED_LOCATION ${FFmpeg_LIBRARY_DIR}/avcodec-AVCODEC_VER.dll
-        IMPORTED_IMPLIB ${FFmpeg_LIBRARY_DIR}/avcodec.lib
-        INTERFACE_INCLUDE_DIRECTORIES ${FFmpeg_INCLUDE_DIR}
+    set_target_properties(avcodec PROPERTIES
+        IMPORTED_LOCATION ${FFMPEG_LIB_DIR}/avcodec-AVCODEC_VER.dll
+        IMPORTED_IMPLIB ${FFMPEG_LIB_DIR}/avcodec.lib
+        INTERFACE_INCLUDE_DIRECTORIES ${FFMPEG_INCLUDE_DIR}
     )
 
-    set_target_properties(FFmpeg::avfilter PROPERTIES
-        IMPORTED_LOCATION ${FFmpeg_LIBRARY_DIR}/avfilter-AVFILTER_VER.dll
-        IMPORTED_IMPLIB ${FFmpeg_LIBRARY_DIR}/avfilter.lib
-        INTERFACE_INCLUDE_DIRECTORIES ${FFmpeg_INCLUDE_DIR}
+    set_target_properties(avfilter PROPERTIES
+        IMPORTED_LOCATION ${FFMPEG_LIB_DIR}/avfilter-AVFILTER_VER.dll
+        IMPORTED_IMPLIB ${FFMPEG_LIB_DIR}/avfilter.lib
+        INTERFACE_INCLUDE_DIRECTORIES ${FFMPEG_INCLUDE_DIR}
     )
 else()
-    set(FFmpeg_LIBRARY_DIR ${CMAKE_CURRENT_LIST_DIR}/lib PARENT_SCOPE)
-    set_target_properties(FFmpeg::swscale PROPERTIES
-        IMPORTED_LOCATION ${FFmpeg_LIBRARY_DIR}/libswscale.a
-        INTERFACE_INCLUDE_DIRECTORIES ${FFmpeg_INCLUDE_DIR}
+    set(FFMPEG_LIB_DIR ${CMAKE_CURRENT_LIST_DIR}/lib)
+
+    add_library(swscale  STATIC IMPORTED GLOBAL)
+    add_library(avutil   STATIC IMPORTED GLOBAL)
+    add_library(avcodec  STATIC IMPORTED GLOBAL)
+    add_library(avfilter STATIC IMPORTED GLOBAL)
+
+    set_target_properties(swscale PROPERTIES
+        IMPORTED_LOCATION ${FFMPEG_LIB_DIR}/libswscale.a
+        INTERFACE_INCLUDE_DIRECTORIES ${FFMPEG_INCLUDE_DIR}
     )
 
-    set_target_properties(FFmpeg::avutil PROPERTIES
-        IMPORTED_LOCATION ${FFmpeg_LIBRARY_DIR}/libavutil.a
-        INTERFACE_INCLUDE_DIRECTORIES ${FFmpeg_INCLUDE_DIR}
+    set_target_properties(avutil PROPERTIES
+        IMPORTED_LOCATION ${FFMPEG_LIB_DIR}/libavutil.a
+        INTERFACE_INCLUDE_DIRECTORIES ${FFMPEG_INCLUDE_DIR}
     )
 
-    set_target_properties(FFmpeg::avcodec PROPERTIES
-        IMPORTED_LOCATION ${FFmpeg_LIBRARY_DIR}/libavcodec.a
-        INTERFACE_INCLUDE_DIRECTORIES ${FFmpeg_INCLUDE_DIR}
+    set_target_properties(avcodec PROPERTIES
+        IMPORTED_LOCATION ${FFMPEG_LIB_DIR}/libavcodec.a
+        INTERFACE_INCLUDE_DIRECTORIES ${FFMPEG_INCLUDE_DIR}
     )
 
-    set_target_properties(FFmpeg::avfilter PROPERTIES
-        IMPORTED_LOCATION ${FFmpeg_LIBRARY_DIR}/libavfilter.a
-        INTERFACE_INCLUDE_DIRECTORIES ${FFmpeg_INCLUDE_DIR}
+    set_target_properties(avfilter PROPERTIES
+        IMPORTED_LOCATION ${FFMPEG_LIB_DIR}/libavfilter.a
+        INTERFACE_INCLUDE_DIRECTORIES ${FFMPEG_INCLUDE_DIR}
     )
 endif()
+
+add_library(FFmpeg::swscale  ALIAS swscale)
+add_library(FFmpeg::avutil   ALIAS avutil)
+add_library(FFmpeg::avcodec  ALIAS avcodec)
+add_library(FFmpeg::avfilter ALIAS avfilter)
