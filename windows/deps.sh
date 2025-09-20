ff0011a72802ebe1d932eb789c0e17d71eb37876
crueter <crueter@eden-emu.dev>
2025-09-20 15:39:37 -0400

    install host compiler on arm64
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- windows/deps.sh
+++ windows/deps.sh
@@ -1,14 +1,18 @@
 #!/bin/sh -e
 
 # normal packages
-PACKAGES="make pkgconf diffutils"
+PACKAGES="make pkgconf diffutils mingw-w64-x86_64-gcc"
 
 # mingw packages
 if [ "$ARCH" = arm64 ]; then
-    PACKAGES="mingw-w64-cross-mingwarm64-gcc mingw-w64-clang-aarch64-vulkan-loader mingw-w64-clang-aarch64-vulkan-headers mingw-w64-clang-aarch64-pkg-config"
+    PACKAGES="$PACKAGES mingw-w64-cross-mingwarm64-gcc"
+    for i in vulkan-loader pkg-config vulkan-headers; do
+        PACKAGES="mingw-w64-clang-aarch64-$i $PACKAGES"
+    done
+
 elif [ "$ARCH" = amd64 ]; then
-    for i in nasm gcc SDL2 ffnvcodec-headers vulkan-headers; do
-        PACKAGES="mingw-w64-x86_64-$i $PACKAGES"
+    for i in nasm ffnvcodec-headers vulkan-headers; do
+        PACKAGES="$PACKAGES mingw-w64-x86_64-$i"
     done
 fi
 
