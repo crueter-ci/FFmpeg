84a552bce628c4086c975089f810a429a9721511
crueter <crueter@eden-emu.dev>
2025-10-02 13:24:15 -0400

    typo
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- windows/build.sh
+++ windows/build.sh
@@ -96,7 +96,7 @@ copy_build_artifacts() {
     make install DESTDIR=${OUT_DIR}
     rm -rf $OUT_DIR/{share,lib/pkgconfig}
 
-	push $OUT_DIR/bin
+	pushd $OUT_DIR/bin
 
     mv swscale-*.dll swscale.dll
     mv avutil-*.dll avutil.dll
