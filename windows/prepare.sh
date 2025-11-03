496179122b8dc71cbc6ff4387e2fda3eef148b40
Caio Oliveira <caiooliveirafarias0@gmail.com>
2025-11-03 09:32:44 -0300

    Update prepare.sh

--- windows/prepare.sh
+++ windows/prepare.sh
@@ -9,15 +9,21 @@ VULKAN_SDK=C:/VulkanSDK/$VULKAN_VER
 
 FFNVCODEC_VER=n13.0.19.0
 
-[ ! -f vulkan.exe ] && curl -L https://sdk.lunarg.com/sdk/download/$VULKAN_VER/windows/vulkan-sdk-windowxX64-$VULKAN_VER.exe -o vulkan.exe
+[ ! -f vulkan.exe ] && curl -L "https://sdk.lunarg.com/sdk/download/$VULKAN_VER/windows/vulkan-sdk-windowxX64-$VULKAN_VER.exe" -o vulkan.exe
 ./vulkan.exe --root "$VULKAN_SDK" --accept-licenses --default-answer --confirm-command install
 
 export VULKAN_SDK
 
 echo "-- Installing ffnvcodec..."
 
-git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
+[ ! -d nv-codec-headers ] && git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
 cd nv-codec-headers
+git fetch --all
 git checkout "$FFNVCODEC_VER"
+
 make install PREFIX=/usr/local
+
 cd ..
+
+export FFNVCODEC_SDK='/c/Program Files/Git/usr/local'
+echo "-- FFNVCODEC_SDK exported as $FFNVCODEC_SDK"
