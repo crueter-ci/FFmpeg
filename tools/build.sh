436c953455ee6d2d573720d436f233c16a963f01
crueter <crueter@eden-emu.dev>
2025-11-11 15:58:26 -0500

    erm
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- tools/build.sh
+++ tools/build.sh
@@ -167,10 +167,13 @@ copy_build_artifacts() {
 	if [ "$PLATFORM" = "solaris" ]; then
 		mkdir -p "$OUT_DIR"/lib
 		find . -name "*.a" -exec cp {} "$OUT_DIR"/lib \;
+		ls "$OUT_DIR"/lib -l
+		echo
+	    $MAKE install-headers INSTALL="/usr/bin/install -C" DESTDIR="${OUT_DIR}"
 	else
     	$MAKE install-libs DESTDIR="${OUT_DIR}"
+	    $MAKE install-headers DESTDIR="${OUT_DIR}"
 	fi
-    $MAKE install-headers DESTDIR="${OUT_DIR}"
     rm -rf "$OUT_DIR"/{share,lib/pkgconfig}
 }
 
