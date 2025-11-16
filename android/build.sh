f490fe2a9252835744f81efc35165e19f720a9b7
crueter <crueter@eden-emu.dev>
2025-11-15 20:57:30 -0500

    android fix (#8)
    * What?
    
    Signed-off-by: crueter <crueter@eden-emu.dev>
    
    * tests
    
    Signed-off-by: crueter <crueter@eden-emu.dev>
    
    * fix
    
    Signed-off-by: crueter <crueter@eden-emu.dev>
    
    ---------
    
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- android/build.sh
+++ android/build.sh
@@ -14,28 +14,14 @@ PLATFORM=android
 ARTIFACTS_DIR=$PWD/artifacts
 mkdir -p "$ARTIFACTS_DIR"
 
-REPO=ffmpeg-kit-16KB
+REPO=ffmpeg-kit
 
 echo "-- Cloning..."
-[ ! -d "$REPO" ] && git clone --depth 1 https://github.com/AliAkhgar/$REPO.git
+[ ! -d "$REPO" ] && git clone --depth 1 https://github.com/crueter-ci/$REPO.git
 
 cd $REPO
 
-echo "-- Patching..."
-
-# Version updates
-sed -i 's/eaa68fad9e5d201d42fde51665f2d137ae96baf0/c24e06c2e184345ceb33eb20a15d1024d9fd3497/' scripts/source.sh
-sed -i "s/n6.0/n7.1.1/" scripts/source.sh
-sed -i 's|arthenica/FFmpeg|FFmpeg/FFmpeg|' scripts/source.sh
-sed -i 's|arthenica/libvpx|webmproject/libvpx|' scripts/source.sh
-sed -i 's/v1.13.0/v1.13.1/' scripts/source.sh
-sed -i 's/v0.8.0/v0.10.1/' scripts/source.sh
-
-# fixes
-sed -i 's/--disable-static //' scripts/android/ffmpeg.sh
-sed -i 's/emms.h/emms.asm/g' scripts/android/ffmpeg.sh
-sed -i '/disable-postproc/d' scripts/android/ffmpeg.sh
-sed -i '20i #include <string.h>' android/ffmpeg-kit-android-lib/src/main/cpp/ffprobekit.c
+# echo "-- Patching..."
 
 echo "-- Building..."
 
@@ -48,7 +34,7 @@ echo "-- Building..."
     --disable-arm-v7a \
     --disable-x86-64 \
     --disable-x86 \
-    --no-archive
+    --no-archive || { cat build.log; grep -re 'cmake_minimum_required' src; exit 1; }
 
 echo "-- Packaging..."
 
