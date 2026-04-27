577c36fd0f7fbc596bf2cf4c93b826a8e14dcaa2
crueter <crueter@eden-emu.dev>
2026-04-27 12:27:35 -0400

    Step 3
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- tools/vars.sh
+++ tools/vars.sh
@@ -4,7 +4,7 @@
 
 # In some projects you will want to fetch latest from gh/fj api
 VERSION="8.0.1"
-export COMMIT="65eed0732cadc42b3689788f175d921974f9c074"
+export COMMIT="b6e470467eeb850f1f97ff8e2e380308d8225e92"
 export PRETTY_NAME="FFmpeg"
 export FILENAME="ffmpeg"
 export REPO="FFmpeg/FFmpeg"
