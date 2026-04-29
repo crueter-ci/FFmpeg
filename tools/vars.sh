6d1f76e7291fdfe164ce642e3ae4008fb0f5bf56
crueter <crueter@eden-emu.dev>
2026-04-28 20:24:04 -0400

    Test fork approach
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- tools/vars.sh
+++ tools/vars.sh
@@ -4,14 +4,14 @@
 
 # In some projects you will want to fetch latest from gh/fj api
 VERSION="8.1"
-export COMMIT="bdc0a29204104301f0ce3b02481d7603d8d70577"
+export COMMIT="8e646bcced0b7d391648abcf5b9b9d4c01d156cb"
 export PRETTY_NAME="FFmpeg"
 export FILENAME="ffmpeg"
-export REPO="FFmpeg/FFmpeg"
+export REPO="crueter/FFmpeg"
 export DIRECTORY="FFmpeg-$COMMIT"
 export TAG="n$VERSION"
 export ARTIFACT="$COMMIT.tar.gz"
-export DOWNLOAD_URL="https://github.com/$REPO/archive/$ARTIFACT"
+export DOWNLOAD_URL="https://code.ffmpeg.org/$REPO/archive/$ARTIFACT"
 
 SHORTSHA=$(echo "$COMMIT" | cut -c1-10)
 export VERSION="$VERSION-$SHORTSHA"
